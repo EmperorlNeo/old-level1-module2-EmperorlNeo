af4790d9390acfd4bf02c7e636232b5e8ceb37a4
EmperorlNeo <league@Admins-Mac-mini-2.local>
2018-11-10 11:25:36 -0800

    Intro to Constructors
    Platypus class and runner

---
+++ src/Platypus.java
@@ -0,0 +1,12 @@
+
+public class Platypus {
+	public Platypus(String name2){
+		this.name1 = name2;
+	}
+		private String name1;
+
+		void sayHi() {
+			System.out.println("The platypus " + name1 + " is smarter than your average platypus.");
+		}
+
+}
